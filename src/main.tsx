1755b1470a8e6f2b81a5e3efaf8f80bc55d37268
Pablo Aparicio <pablo.aparicio1996@gmail.com>
2026-03-30 13:39:22 +0200

    Décimo commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import './index.css'
 import App from './App'
 
 // Lock to portrait on supported browsers (Android Chrome, etc.)
-screen.orientation?.lock?.('portrait').catch(() => {})
+// screen.orientation?.lock?.('portrait').catch(() => {})
+screen.orientation?.lock?.('portrait-primary').catch(() => {})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
